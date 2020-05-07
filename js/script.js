aa17c4f99709c74625716e223d8c520825d3a6cf
Saksham Dhull <saksham18186@iiitd.ac.in>
2020-05-07 23:11:52 +0530

    Fixed Dark Mode Button

--- js/script.js
+++ js/script.js
@@ -2,33 +2,16 @@ var checkbox=document.querySelector('input[name=theme]');
 checkbox.addEventListener('change', function(){
 	console.log(document.documentElement.getAttribute('data-theme'));
 	if (this.checked){
-		trans();
+		// trans();
 		document.documentElement.setAttribute('data-theme', 'dark');
 	}
 	else{
-		trans();
+		// trans();
 		document.documentElement.setAttribute('data-theme', 'light');
 	}
-})
-let trans=() => {
-	document.documentElement.classList.add('transition');
-	window.setTimeout(()=>{
-		document.documentElement.classList.remove('transition')
-	},1000)
-}
+});
 
-function DarkMode()
-{
-	console.log(document.body.style.background);
-	if (document.body.style.backgroundColor=="#0a192f")
-	{
-		document.body.style.backgroundColor="white";
-	}
-	else{
-		document.body.style.backgroundColor="#0a192f";
-	}
-	
-}
+
 ScrollReveal().reveal('.section-heading',{distance:"100px",duration:1500});
 ScrollReveal().reveal('#Aboutme1',{distance:"100px",duration:1500});
 ScrollReveal().reveal('#Aboutme2',{distance:"100px",duration:1500});
@@ -59,6 +42,3 @@ $(function(){
 		scroll=$(document).scrollTop();
 	});
 });
-// $('colorButton').on('click',function(){
-// 	$('body').css('background-color',"#0a192f");
-// });
