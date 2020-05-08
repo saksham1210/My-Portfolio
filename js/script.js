31d6231684ba3a1aee60ba1781ca113a1920d08d
Saksham Dhull <saksham18186@iiitd.ac.in>
2020-05-08 20:42:58 +0530

    Final changes

--- js/script.js
+++ js/script.js
@@ -19,26 +19,5 @@ ScrollReveal().reveal('#project1',{distance:"100px",duration:1500});
 ScrollReveal().reveal('#project2',{distance:"100px",duration:1500});
 ScrollReveal().reveal('#project3',{distance:"100px",duration:1500});
 ScrollReveal().reveal('#Contactme',{distance:"100px",duration:1500});
-$(function(){
-	var scroll = $(document).scrollTop();
-	var navHeight =$(".navbarScroll").outerHeight();
-	$(window).scroll(function(){
-		var scrolled =$(document).scrollTop();
-		if (scrolled>navHeight)
-		{
-			$(".navbarScroll").addClass("animate");
-		}
-		else{
-			$(".navbarScroll").removeClass("animate");
-		}
-		if(scrolled>scroll)
-		{
-			$(".navbarScroll").removeClass("sticky-top");
-		}
-		else
-		{
-			$(".navbarScroll").addClass("sticky-top");
-		}
-		scroll=$(document).scrollTop();
-	});
-});
+
+
